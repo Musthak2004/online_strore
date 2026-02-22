bbea7db07e26e03978b4df12797911a69471dd2d
Musthak <musthakcool55@gmail.com>
2026-02-22 21:35:53 +0530

    Next need to migrate

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import CustomUser
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+
+    list_display = ["email", "username", "phone_number", "is_staff", "is_active"]
+
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('email', 'phone_number', 'is_vendor',)}),
+    )
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('email', 'phone_number', 'is_vendor',)}),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
